e183135cf61684132cc0cbeac1a5edfc572bc883
Pnski <s1l3nce@hotmail.de>
2025-12-01 17:47:06 +0100

    _update icon to Tnl

--- observablehq.config.js
+++ observablehq.config.js
@@ -17,14 +17,14 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: '<link rel="icon" href="TnlIcon.png" type="image/png" sizes="32x32">',
 
   // The path to the source root.
   root: "src",
 
   // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
-  // header: "", // what to show in the header (HTML)
+  theme: "dark", // try "light", "dark", "slate", etc.
+  header: "TnL Dps Stats", // what to show in the header (HTML)
   // footer: "Built with Observable.", // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
